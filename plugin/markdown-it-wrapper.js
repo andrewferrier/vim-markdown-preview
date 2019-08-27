e32589a1ff188f81049630ad88caa4a5c4b36801
Andrew Ferrier <andrewferrier@example.com>
2019-08-27 22:09:43 +0100

    Improve JavaScript style.

--- plugin/markdown-it-wrapper.js
+++ plugin/markdown-it-wrapper.js
@@ -1,50 +1,56 @@
 #!/usr/bin/env node
 
-'use strict'
+/* eslint-env node */
 
-const md = require('markdown-it')({
-  html: true,
-  typographer: true
-})
+"use strict";
+
+const md = require("markdown-it")({
+  "html": true,
+  "typographer": true
+});
 
 // See https://github.com/markdown-it/markdown-it/issues/117#issuecomment-109386469
-md.renderer.rules.table_open = function (tokens, idx) {
-  return '<table class="table">'
-}
+/* eslint camelcase: ["off"] */
+md.renderer.rules.table_open = function table_open(tokens, idx) {
+  return "<table class=\"table\">";
+};
 
-md.use(require('markdown-it-table-of-contents'), {
-  includeLevel: [1, 2, 3]
-})
+md.use(require("markdown-it-table-of-contents"), {
+  /* eslint no-magic-numbers: ["error", { "ignore": [1, 2, 3] }] */
+  "includeLevel": [1, 2, 3]
+});
 
-md.use(require('markdown-it-anchor'))
+md.use(require("markdown-it-anchor"));
 
-function readFile (encoding, callback) {
+const readFile = (encoding, callback) => {
   // read from stdin
-  var chunks = []
+  const chunks = [];
 
-  process.stdin.on('data', function (chunk) {
-    chunks.push(chunk)
-  })
+  process.stdin.on("data", (chunk) => {
+    chunks.push(chunk);
+  });
 
-  process.stdin.on('end', function () {
-    return callback(null, Buffer.concat(chunks).toString(encoding))
-  })
-}
+  process.stdin.on("end", () => {
+    return callback(null, Buffer.concat(chunks).toString(encoding));
+  });
+};
 
-readFile('utf8', function (err, input) {
-  var output
+readFile("utf8", (err, input) => {
+  let output;
 
   if (err) {
-    console.error(err.stack || err.message || String(err))
-    process.exit(1)
+    const errMessage = err.stack || err.message || String(err);
+
+    throw new Error(errMessage);
   }
 
   try {
-    output = md.render(input)
-  } catch (e) {
-    console.error(e.stack || e.message || String(e))
-    process.exit(1)
+    output = md.render(input);
+  } catch (err2) {
+    const errMessage = err2.stack || err2.message || String(err2);
+
+    throw new Error(errMessage);
   }
 
-  process.stdout.write(output)
-})
+  process.stdout.write(output);
+});
